cd56dd24b196d14998af20e4295fbba3ec00e284
hu-simon <definefunction@gmail.com>
2014-09-09 22:11:20 -0700

    Recommit

---
+++ src/QuadDriver.java
@@ -0,0 +1,38 @@
+//We first import two important classes that we're going to be using throughout the code 
+//First, the scanner class so that we can ask the user for input
+//Second, the Math class, so that we may use the .sqrt() method
+import java.lang.Math;
+import java.util.Scanner;
+
+public class QuadDriver {
+	public static void main(String[] args) {
+	
+		Quadratic formula = new Quadratic();
+		Scanner getNumber = new Scanner(System.in);
+		
+		//We start by asking the user for the first coefficient, a.
+		System.out.println("What is the first coefficient? ");
+		formula.firstCoefficient = getNumber.nextInt();
+		
+		//Now we're going to ask for the second coefficient, b.
+		System.out.println("What is the second coefficient? ");
+		formula.secondCoefficient = getNumber.nextInt();
+		
+		//Finally, we're going to ask the user for the third coefficient, c.
+		System.out.println("What is the third coefficient? ");
+		formula.thirdCoefficient = getNumber.nextInt();
+		
+		//Now, we're going to evaluate those numbers and plug them into the formula. 
+		int firstStep = -formula.secondCoefficient;
+		double secondStep = -formula.secondCoefficient + Math.sqrt((formula.secondCoefficient * formula.secondCoefficient) - 4 * (formula.firstCoefficient * formula.thirdCoefficient));
+		double secondStepTwo = -formula.secondCoefficient - Math.sqrt((formula.secondCoefficient * formula.secondCoefficient) - 4 * (formula.firstCoefficient * formula.thirdCoefficient));  
+		double thirdStep = secondStep / (2 * formula.firstCoefficient);
+		double thirdStepTwo = secondStepTwo / (2 * formula.firstCoefficient);
+		
+		//Now, we tell the user the results.
+		System.out.println("Your two answers are: " + thirdStep + " and " + thirdStepTwo);
+		
+		//That's it! We now thank the user for trying the product. 
+		System.out.println("Thank you for using the Quadratic Formula Calculator! Please come again!");
+	}
+}
---
+++ src/Quadratic.java
@@ -0,0 +1,14 @@
+public class Quadratic {
+	int firstCoefficient;
+	int secondCoefficient;
+	int thirdCoefficient;
+	int firstStep; 
+	double secondStep;
+	double secondStepTwo;
+	double thirdStep;
+	double thirdStepTwo;
+	
+	public Quadratic() {
+		
+	}
+}
